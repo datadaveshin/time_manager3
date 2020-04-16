5f7b64ebb3f6165a0d90e6cbb9f3e96c1a99388c
David Shin <davidshin444@gmail.com>
2020-04-15 18:39:55 -0700

    v7 Edit test, add pages#home view
    Test passed, route works in browser

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -4,7 +4,7 @@ describe 'navigate to' do
   describe 'home' do
     it 'can be reached successfully' do
       visit root_path
-      expect(pages.status_code).to eq(200)
+      expect(page.status_code).to eq(200)
     end
   end
 end
