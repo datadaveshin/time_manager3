ff85bf3c3cb33ae588950a31284b375743780b19
David Shin <davidshin444@gmail.com>
2020-04-15 18:29:35 -0700

    v7 Minor edits to test, get route to pass

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'navigate to' do
   describe 'home' do
     it 'can be reached successfully' do
-      visit_root_path
-      expect(page.status_code).to eq(200)
+      visit root_path
+      expect(pages.status_code).to eq(200)
     end
   end
 end
