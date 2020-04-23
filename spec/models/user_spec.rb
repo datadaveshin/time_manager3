564020d12bd122c0964c259bcdf4bc35b5f68648
David Shin <davidshin444@gmail.com>
2020-04-23 16:57:57 -0700

    v10 Add first basic User create test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".User" do
+    it "#create" do
+      user = User.create(email: "test@example.com", password: "qwerty", password_confirmation: "qwerty")
+      expect(user).to be_valid
+    end
+  end
+
+  after(:all) { User.destroy_all }
 end
