381ccdd97546e9cbfa9b0724f45d271faea0ec66
David Shin <davidshin444@gmail.com>
2020-04-24 14:44:19 -0700

    v12 post#index action and view

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 describe 'Navigation' do
-  describe 'home#index' do
+  describe 'post#index' do
     it 'can be reached successfully' do
-      visit root_path
+      visit posts_path
       expect(page.status_code).to eq(200)
     end
+  end
+
+  it 'has Post title' do
+    visit posts_path
+    expect(page).to have_content(/Posts/)
   end
 end
