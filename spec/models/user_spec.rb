7772eb1693a1c1474ceecbdf136a527c1bffbd9c
David Shin <davidshin444@gmail.com>
2020-04-23 17:14:53 -0700

    v10 Add user first name, last name validations, update tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe ".User" do
-    it "#create" do
-      user = User.create(email: "test@example.com", password: "qwerty", password_confirmation: "qwerty")
-      expect(user).to be_valid
+  describe ".create" do
+    before do
+      @user = User.create(
+        email: "test@example.com",
+        password: "qwerty",
+        password_confirmation: "qwerty",
+        first_name: "John",
+        last_name: "Doe"
+      )
+    end
+
+    it "works" do
+      expect(@user).to be_valid
+    end
+
+    it "can't create without first_name" do
+      @user.first_name = nil
+      expect(@user.first_name).to be_falsey
+    end
+
+    it "can't create without last_name" do
+      @user.last_name = nil
+      expect(@user.last_name).to be_falsey
     end
   end
 
