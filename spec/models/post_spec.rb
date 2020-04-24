38e95918c94871cf7c5219bc563cba7bbec7bb94
David Shin <davidshin444@gmail.com>
2020-04-23 17:57:58 -0700

    v11 Add validations & tests for creating Posts, delete helper file

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create" do
+    before do
+      @post = Post.create(date: Date.today, reason: "shipment late")
+    end
+
+    it "works" do
+      expect(@post).to be_valid
+    end
+
+    it "cannot be done without a date" do
+      @post.date = nil
+      expect(@post.date).to be_falsey
+    end
+
+    it "cannot be done without a reason" do
+      @post.reason = nil
+      expect(@post.reason).to be_falsey
+    end
+  end
+  after (:all) {Post.destroy_all}
 end
