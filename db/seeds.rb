fc73fda24544e32261af6f650d7a5227f31a8a7e
David Shin <davidshin444@gmail.com>
2020-04-25 14:40:31 -0700

    v13 Made seeds file for Posts
    Run with:
    ```
    bin/rails db:setup
    ```

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |post|
+  Post.create!(date: Date.today, reason: "#{post} This is my reason")
+end
+puts "created 100 posts"
