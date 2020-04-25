65d65eddde651e321420af30311e9838b86cec53
David Shin <davidshin444@gmail.com>
2020-04-25 16:12:51 -0700

    v14 BDD to create Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :reason))
+
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    # logger.log(@post.inspect)
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,15 +1,34 @@
 require 'rails_helper'
 
 describe 'Navigation' do
-  describe 'post#index' do
+  describe 'Post#index' do
     it 'can be reached successfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
+
+    it 'has Post title' do
+      visit posts_path
+      expect(page).to have_content(/Posts/)
+    end
   end
 
-  it 'has Post title' do
-    visit posts_path
-    expect(page).to have_content(/Posts/)
+  describe 'Post#new' do
+    it 'form can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'Post#create' do
+    it 'works' do
+      visit new_post_path
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[reason]', with: "This is a reason"
+
+      click_on "Save"
+
+      expect(page).to have_content("This is a reason")
+    end
   end
 end
