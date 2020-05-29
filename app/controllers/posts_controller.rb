eebd9dbb9e0d16d522bea5a9c3e6807ada05c09f
David Shin <davidshin444@gmail.com>
2020-05-28 19:46:30 -0700

    v16 - Add User and authentication so only signed in users can post
    User connection generated with:
    ```
    rails g migration add_users_to_posts users:references
    ```

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :show]
+  before_action :set_post, only: [:show]
+
   def index
   end
 
@@ -8,7 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(params.require(:post).permit(:date, :reason))
-
+    @post.user_id = current_user.id
     @post.save
 
     redirect_to @post
@@ -17,5 +20,14 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     # logger.log(@post.inspect)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:date, :reason, :user_id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'Navigation' do
+
   describe 'Post#index' do
     it 'can be reached successfully' do
       visit posts_path
@@ -14,30 +15,30 @@ describe 'Navigation' do
   end
 end
 
+User.destroy_all
 describe 'Creation' do
-  before do
-    user = User.create(
-      email: "jdoe@example.com",
-      password: "12345678",
-      password_confirmation: "12345678",
-      first_name: "John",
-      last_name: "Doe"
-    )
-    login_as(user, scope: "user")
-    visit new_post_path
-  end
-
   describe 'Post#new' do
+    before do
+      User.destroy_all # NOTE DSS REMOVE ME LATER
+      user = User.create!(
+        email: "jdoe@example.com",
+        password: "12345678",
+        password_confirmation: "12345678",
+        first_name: "John",
+        last_name: "Doe"
+      )
+
+      login_as(user, :scope => :user)
+      visit new_post_path
+    end
+
     it 'form can be reached' do
       expect(page.status_code).to eq(200)
     end
-  end
 
-  describe 'Post#create' do
     it 'works' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[reason]', with: "This is a reason"
-
       click_on "Save"
 
       expect(page).to have_content("This is a reason")
@@ -45,9 +46,11 @@ describe 'Creation' do
 
     it 'will have an associated User' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[reason]', with: "User_Association"
+      fill_in 'post[reason]', with: "User Association"
+      click_on "Save"
 
-      expect(User.last.posts.last.reason).to_eq("User_Association")
+      expect(User.last.posts.last.reason).to eq("User Association")
     end
   end
+  # after(:all) { User.destroy_all } # NOTE DSS REMOVE ME LATER
 end
